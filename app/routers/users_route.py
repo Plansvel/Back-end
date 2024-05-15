8a387cff4f5c0ae3e31cd88ce4ed7ee535ab83d7
Kawan Ximendes de Lima <kawan.ximendes@gmail.com>
2024-05-15 00:44:49 -0300

    feat: Route to register a user

---
+++ app/routers/users_route.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter, status, HTTPException
+from schemas import user_schema
+from services import user_service
+
+router = APIRouter(prefix="/users",
+                   tags=["User"],
+                   responses={400: {"description": "Not found"}})
+
+@router.post('', status_code=status.HTTP_201_CREATED, response_model=user_schema.User)
+async def register_user(user: user_schema.UserCreate):
+    try:
+        user_created = user_service.create_user(user)
+        return user_created.data[0]
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
