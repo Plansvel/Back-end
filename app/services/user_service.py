0e894cbf134008556c862c6890f27a5f8076f3a2
Kawan Ximendes de Lima <kawan.ximendes@gmail.com>
2024-05-16 00:39:26 -0300

    feat: Added a def for to create the role of user

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -24,10 +24,29 @@ def create_user(user: user_schema.UserCreate):
             }).execute()
         
         if user_created:
-            return user_created
+            user_created_copy = dict(user_created.data[0])
+            user_created_copy["funcao"] = create_role(user_created.data[0]["id_usuario"])
+            user_response = user_schema.User(**user_created_copy)
+            return user_response
         else:
             raise HTTPException(status_code=412, detail="User creation failed")
     
     except HTTPException as http_exception:
         raise http_exception
-    
+
+def create_role(id_user: int):
+    try:
+        
+        connection.table("usuario_funcao").insert({
+            "id_usuario": id_user,
+            "id_funcao": 1,
+        }).execute()
+        
+        id_role = connection.table("usuario_funcao").select("id_funcao").eq("id_usuario", id_user).execute()
+        
+        funcao = connection.table("funcao").select("nome_funcao").eq("id_funcao", id_role.data[0]["id_funcao"]).execute()
+        
+        return funcao.data[0]["nome_funcao"]
+        
+    except HTTPException as http_exception:
+        raise http_exception
